c29d8c6276614445833ef3407d0c5851d3d0cb8f
Lumala Brian <lumalabravo@gmail.com>
2024-05-29 19:29:00 +0300

    looping and maping in picker to improve

---
+++ picker/src/components/constants.js
@@ -0,0 +1,1 @@
+export const difficulties = ["Low", "Medium", "High", "Insane"];
--- picker/src/components/menuList/MenuList.jsx
+++ picker/src/components/menuList/MenuList.jsx
@@ -1,30 +1,21 @@
 import React from "react";
+import { difficulties } from "../constants";
 import MenuListItem from "../menuListItem/MenuListItem";
 import s from "./style.module.css";
 
 const ParentComponent = (props) => {
   return (
     <div className={s.container}>
-      <MenuListItem
-        isSelected={props.difficulty === "Low"}
-        difficulty="Low"
-        itemClick={props.itemClick}
-      />
-      <MenuListItem
-        isSelected={props.difficulty === "Medium"}
-        difficulty="Medium"
-        itemClick={props.itemClick}
-      />
-      <MenuListItem
-        isSelected={props.difficulty === "High"}
-        difficulty="High"
-        itemClick={props.itemClick}
-      />
-      <MenuListItem
-        isSelected={props.difficulty === "Insane"}
-        difficulty="Insane"
-        itemClick={props.itemClick}
-      />
+      {difficulties.map((difficulty, index) => {
+        return (
+          <MenuListItem
+            key={index}
+            isSelected={props.difficulty === difficulty}
+            difficulty={difficulty}
+            itemClick={props.itemClick}
+          />
+        );
+      })}
     </div>
   );
 };
