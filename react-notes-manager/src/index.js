b1e8f55f2f421de4b414fc59d15c4750c349b8fd
Lumala Brian <lumalabravo@gmail.com>
2024-06-04 15:08:49 +0300

    persisitng the user

--- react-notes-manager/src/index.js
+++ react-notes-manager/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import { ProtectedApp } from "./App";
 import "./index.css";
 import Note from "./pages/Note";
@@ -11,25 +12,27 @@ import PageNotFound from "./pages/PageNotFound";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import { FirebaseApp } from "./service/firebase";
-import { store } from "./store";
+import { persistor, store } from "./store";
 
 FirebaseApp.init(); // before doing anything, we first establish connection with firebase
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={<ProtectedApp />}>
-            <Route path="/" element={<NoteBrowse />} />
-            <Route path="/notes/:id" element={<Note />} />
-            <Route path="/note/new" element={<NoteCreate />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/" element={<ProtectedApp />}>
+              <Route path="/" element={<NoteBrowse />} />
+              <Route path="/notes/:id" element={<Note />} />
+              <Route path="/note/new" element={<NoteCreate />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- react-notes-manager/src/store/index.js
+++ react-notes-manager/src/store/index.js
@@ -1,10 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { authReducer } from "./auth/authSlice";
 import { noteReducer } from "./notes/note_slice";
 
-export const store = configureStore({
-  reducer: {
-    noteSlice: noteReducer,
-    authSlice: authReducer,
-  },
+const rootReducer = combineReducers({
+  noteSlice: noteReducer,
+  authSlice: authReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["authSlice"],
+};
+
+const persistedReducers = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { persistor, store };
