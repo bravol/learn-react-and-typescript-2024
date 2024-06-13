244e60679aad1b045c609166c6a6342a0a05471c
Brian Bravol <lumalabravo@gmail.com>
2024-06-13 12:21:24 +0300

    seeting lottie animation for a right and correct answer

--- react-typescript/react-typescript-quiz/src/features/Play.tsx
+++ react-typescript/react-typescript-quiz/src/features/Play.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QuizItem } from "../types/quiz_types";
 import {
   Box,
@@ -8,6 +8,9 @@ import {
   RadioGroup,
   SimpleGrid,
 } from "@chakra-ui/react";
+import Lottie from "lottie-react";
+import ValidAnimation from "../assets/lottie/valid.json";
+import InValidAnimation from "../assets/lottie/invalid.json";
 
 type Props = {
   quiz: QuizItem[];
@@ -27,20 +30,50 @@ const Play = (props: Props) => {
       </Radio>
     );
   });
+  const [answer, setAnswer] = useState<string>("");
+  const [questionState, setQuestionState] = useState<
+    "valid" | "invalid" | "unanswered"
+  >("unanswered");
+
+  function isValidAnswer(answer: string) {
+    return answer === currentQuizItem.correct_answer;
+  }
+
+  useEffect(() => {
+    if (answer) {
+      if (isValidAnswer(answer)) {
+        setQuestionState("valid");
+      } else {
+        setQuestionState("invalid");
+      }
+    }
+  }, [answer]);
   return (
     <Box margin={5}>
       <Flex direction={"column"} alignItems={"center"} justify={"center"}>
         <Heading fontSize={"3xl"} mt={100} mb={20}>
           {currentQuizItem.question}
         </Heading>
-        <RadioGroup
-          value={currentQuizItemIndex.toString()}
-          onChange={() => setCurrentQuizItemIndex(currentQuizItemIndex + 1)}
-        >
+        <RadioGroup value={answer} onChange={setAnswer}>
           <SimpleGrid column={2} spacing={4}>
             {radioLists}
           </SimpleGrid>
         </RadioGroup>
+        <Lottie
+          loop={false}
+          style={{ marginTop: 100, height: 150 }}
+          animationData={
+            questionState === "unanswered"
+              ? null
+              : questionState === "valid"
+              ? ValidAnimation
+              : InValidAnimation
+          }
+          onComplete={() => {
+            setQuestionState("unanswered");
+            setCurrentQuizItemIndex(currentQuizItemIndex + 1);
+          }}
+        />
       </Flex>
     </Box>
   );
