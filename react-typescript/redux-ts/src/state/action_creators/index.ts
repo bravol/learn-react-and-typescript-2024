940c70bacc3b97c8c8e0e95556648e503cdf2b84
Brian Bravol <lumalabravo@gmail.com>
2024-06-17 15:05:35 +0300

    undates , faetch data using axios

---
+++ react-typescript/redux-ts/src/state/action_creators/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { ActionType } from "../action_types";
+import { Action } from "../actions";
+import { Dispatch } from "react";
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionType.SEARCH_REPOSITORIES });
+    try {
+      const { data } = await axios.get(
+        `https://registry.npmjs.org/-/v1/search`,
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: error.message,
+      });
+    }
+  };
+};
